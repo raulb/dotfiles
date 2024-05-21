f602cccb66e74b76cffc646528ac64052ab0deb0
Raúl Barroso <ra.barroso@gmail.com>
2024-05-21 10:16:08 +0200

    update dotfiles

--- java/install.sh
+++ java/install.sh
@@ -1,3 +1,3 @@
-# https://mkyong.com/java/how-to-install-java-on-mac-osx/
-#sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
-sudo ln -sfn /usr/local/opt/openjdk@21/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-21.jdk
+# # https://mkyong.com/java/how-to-install-java-on-mac-osx/
+# #sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+# sudo ln -sfn /usr/local/opt/openjdk@21/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-21.jdk
---
+++ mise/path.zsh
@@ -0,0 +1,2 @@
+# export PATH="$HOME/bin:$PATH"
+# eval "$(mise activate zsh --shims)"
